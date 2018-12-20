89ac3260f32368eba82e1f4ea9894f0cfcd4426c
zhangh <zhangh@sanwayworld.com>
2018-12-20 17:33:30 +0800

    12.20

--- src/main/java/com/zhweb/controller/MMovieController.java
+++ src/main/java/com/zhweb/controller/MMovieController.java
@@ -5,6 +5,8 @@ import com.zhweb.entity.MMovie;
 import com.zhweb.service.MMovieService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/mMovie")
 public class MMovieController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private MMovieService mMovieService;
 
